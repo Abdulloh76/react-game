85310c245b29305c933ea5ead190b5e2df72529f
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2021-02-21 23:13:51 +0500

    feat: implement function for generating array for game board

---
+++ src/utils/generateBoardArray.ts
@@ -0,0 +1,42 @@
+function generateBoardArray(width: number, height: number, minesNumber: number) {
+  const arr: number[][] = Array.from(Array(height), () => Array(width).fill(0));
+  let remainingMines: number = minesNumber;
+
+  while (remainingMines !== 0) {
+    const x: number = Math.floor(Math.random() * width);
+    const y: number = Math.floor(Math.random() * height);
+
+    if (arr[x][y] !== -1) {
+      arr[x][y] = -1;
+      remainingMines -= 1;
+
+      if (x + 1 < width) {
+        if (y + 1 < height && arr[x + 1][y + 1] !== -1) arr[x + 1][y + 1] += 1;
+        if (arr[x + 1][y] !== -1) arr[x + 1][y] += 1;
+        if (y - 1 >= 0 && arr[x + 1][y - 1] !== -1) arr[x + 1][y - 1] += 1;
+      }
+      if (x - 1 >= 0) {
+        if (y + 1 < height && arr[x - 1][y + 1] !== -1) arr[x - 1][y + 1] += 1;
+        if (arr[x - 1][y] !== -1) arr[x - 1][y] += 1;
+        if (y - 1 >= 0 && arr[x - 1][y - 1] !== -1) arr[x - 1][y - 1] += 1;
+      }
+      if (y + 1 < height && arr[x][y + 1] !== -1) arr[x][y + 1] += 1;
+      if (y - 1 >= 0 && arr[x][y - 1] !== -1) arr[x][y - 1] += 1;
+    }
+  }
+  return arr;
+}
+
+export default generateBoardArray;
+
+// generateBoardArray(9, 9, 10) ~~
+// [ 1,  1,  1,  0,  0,  1,  1,  1,  0]
+// [ 1, -1,  2,  1,  1,  2, -1,  3,  1]
+// [ 2,  2,  2, -1,  2,  3, -1, -1,  1]
+// [-1,  1,  1,  2, -1,  2,  2,  2,  1]
+// [ 2,  2,  0,  1,  1,  1,  0,  0,  0]
+// [-1,  1,  0,  0,  0,  0,  0,  0,  0]
+// [ 2,  2,  1,  0,  0,  0,  0,  0,  0]
+// [ 1, -1,  2,  1,  1,  0,  0,  0,  0]
+// [ 1,  1,  2, -1,  1,  0,  0,  0,  0]
+// -1 === mine
