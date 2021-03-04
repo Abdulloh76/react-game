ab0e2170b131cda05cd654268cc29e7ff0b69ecd
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2021-03-04 09:53:21 +0500

    feat: implement base logic and styles for modal component

---
+++ src/componenets/Modal.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react'
+
+interface Props {
+  children: JSX.Element;
+  show: boolean;
+  setShow: (value: boolean) => void;
+}
+
+export default function Modal({ children, show, setShow }: Props) {
+  const closeByEsc = (e: KeyboardEvent) => {
+    if (e.keyCode === 27) {
+      setShow(false);
+    }
+  }
+  
+  useEffect(() => {
+    document.addEventListener('keydown', closeByEsc)
+    return () => document.removeEventListener('keydown', closeByEsc)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (!show) return null
+  
+  return (
+    <div className="modal" onClick={() => setShow(false)}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-header">
+          <h4 className="modal-title">Modal Title</h4>
+        </div>
+        <div className="modal-body">{children}</div>
+        <div className="modal-footer">
+          <button className="close-modal" onClick={() => setShow(false)}></button>
+        </div>
+      </div>
+    </div>
+  )
+}
