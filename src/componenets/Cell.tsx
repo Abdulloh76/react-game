d0b1a53ceeff5a60799b6bb8fd30e1fe48d0b17e
Abdullokh Musaev <musayevabdulloh76@gmail.com>
2021-03-04 12:06:45 +0500

    feat: implement guessing feature

--- src/componenets/Cell.tsx
+++ src/componenets/Cell.tsx
@@ -1,32 +1,55 @@
-import React, { useContext } from 'react';
-import { ReactComponent as Mine } from '../assets/mine.svg'
+import React, { useContext, useState } from 'react';
+import { ReactComponent as Mine } from '../assets/mine.svg';
+import { ReactComponent as RedFlag } from '../assets/redflag.svg';
 import { BoardContext } from './Board';
+import { GlobalContext, GlobalState } from './GlobalOptions';
 
 interface Props {
   data: number;
   x: number;
-  y:number;
+  y: number;
 }
 
-export default function Cell({ data, x, y }:Props) {
-  const [trackingArray, cellClick] = useContext(BoardContext)
+export default function Cell({ data, x, y }: Props) {
+  const [trackingArray, cellClick] = useContext(BoardContext);
+  const { toggler } = useContext(GlobalContext) as GlobalState;
+
+  const [flag, setFlag] = useState(Boolean(trackingArray[x][y] === -1));
 
   const content = (value: number) => {
     if (value === -1) return <Mine />;
-    else if(value === 0) return <p></p>
+    else if (value === 0) return <p></p>;
     return <p>{value}</p>;
   };
 
+  const putFlag = () => (flag ? <RedFlag /> : null);
+
   const clickHandler = () => {
-    if (data === -1) console.log('you clicked the mine');
-    cellClick(x, y)
-    trackingArray[x][y]=1;
-    localStorage.setItem('trackingArray', JSON.stringify(trackingArray))
-  }
+    if (!toggler) {
+      setFlag(!flag);
+      trackingArray[x][y] = flag ? 0 : -1;
+    } else {
+      if (data === -1) console.log('you clicked the mine');
+      cellClick(x, y);
+      trackingArray[x][y] = 1;
+      localStorage.setItem('trackingArray', JSON.stringify(trackingArray));
+    }
+  };
 
   return (
-    <div className={`board__cell ${Boolean(trackingArray[x][y])?'cell-visited':''}`} onClick={clickHandler}>
+    <div
+      className={`board__cell ${
+        Boolean(trackingArray[x][y] === 1) ? 'cell-visited' : ''
+      }`}
+      onClick={clickHandler}
+      onContextMenu={(e) => {
+        e.preventDefault()
+        setFlag(!flag);
+        trackingArray[x][y] = flag ? 0 : -1;
+      }}
+    >
       {content(data)}
+      {putFlag()}
     </div>
   );
 }
--- src/componenets/Toggler.tsx
+++ src/componenets/Toggler.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { ReactComponent as Mine } from '../assets/mine.svg'
+import { ReactComponent as RedFlag } from '../assets/redflag.svg'
+import { GlobalContext, GlobalState } from './GlobalOptions'
 
 export default function Toggler() {
-
+  const { toggler, changeToggler } = useContext(GlobalContext) as GlobalState
+  // true->mine, false->flag
+  const component = () => toggler ? <Mine /> : <RedFlag />
   return (
-    <div className="header__toggler">
-      <Mine />
+    <div className="header__toggler" onClick={() => changeToggler(!toggler)}>
+      {component()}
     </div>
   )
 }
